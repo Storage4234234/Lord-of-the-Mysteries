86e578415ab6fff5bd0b9727f6cc06339e7ce2be
SwimmingTuna <54822683+SwimmingTuna@users.noreply.github.com>
2025-03-27 23:43:54 -0400

    potion recipes now working

--- src/main/java/net/swimmingtuna/lotm/util/BeyonderUtil.java
+++ src/main/java/net/swimmingtuna/lotm/util/BeyonderUtil.java
@@ -1544,42 +1544,49 @@ public class BeyonderUtil {
     }
 
     public static void registerAllRecipes(CommandContext<CommandSourceStack> server) {
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_9_potion bossominium:flower_of_genesis bossominium:redstone_hard_drive minecraft:rotten_flesh alexscaves:charred_remnant bosses_of_mass_destruction:soul_star");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_8_potion legendary_monsters:frozen_rune mutantmonsters:hulk_hammer alexscaves:sweet_tooth minecraft:netherite_scrap legendary_monsters:crystal_of_sandstorm");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_7_potion alexscaves:pure_darkness bossominium:soul_eye arphex:giant_spinneret the_deep_void:ash_pile bossominium:dead_charm");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_6_potion minecraft:nether_star bosses_of_mass_destruction:void_thorn kom:nectra_egg cataclysm:monstrous_horn illageandspillage:bag_of_horrors");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_5_potion soulsweapons:chaos_crown cataclysm:witherite_ingot kom:anglospike arphex:crusher_claw alexscaves:immortal_embryo");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_4_potion iceandfire:dragon_skull_fire cataclysm:ignitium_ingot eeeabsmobs:guardian_core mythsandlegends:umbras_whisper macabre:gargamaw_heart");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_3_potion minecraft:nether_star iceandfire:dragon_skull_ice arphex:abyssal_crystal");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_2_potion terramity:giant_sniffers_hoof soulsweapons:lord_soul_day_stalker soulsweapons:lord_soul_night_prowler minecraft:iron_ingot");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_1_potion terramity:music_sheet_of_the_omnipotent_ultra_sniffer minecraft:netherite_block");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_9_potion bossominium:rusted_trident mowziesmobs:sol_visage aquamirae:fin arphex:roach_nymph arphex:fly_appendage");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_8_potion bossominium:mossy_stone_tablet alexsmobs:warped_muscle iceandfire:sea_serpent_fang minecraft:prismarine_shard mutantmonsters:endersoul_hand");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_7_potion eeeabsmobs:heart_of_pagan mowziesmobs:ice_crystal aquamirae:abyssal_amethyst the_deep_void:tendrils arphex:necrotic_fang");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_6_potion illageandspillage:spellbound_book kom:caligan_saw arphex:oversized_stinger minecraft:dragon_egg bosses_of_mass_destruction:void_thorn");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_5_potion aquamirae:frozen_key soulsweapons:essence_of_eventide cataclysm:gauntlet_of_guard alexscaves:immortal_embryo arphex:void_geode_shard");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_4_potion iceandfire:dragon_skull_ice alexscaves:tectonic_shard cataclysm:abyssal_egg macabre:baal_heart soulsweapons:darkin_blade");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_3_potion soulsweapons:essence_of_luminescence iceandfire:dragon_skull_lightning arphex:void_geode");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_2_potion terramity:angel_feather soulsweapons:lord_soul_day_stalker soulsweapons:lord_soul_night_prowler minecraft:lightning_rod");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_1_potion terramity:music_sheet_of_the_omnipotent_ultra_sniffer minecraft:diamond_block");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_9_potion bossominium:golden_shard bossominium:forest_core born_in_chaos_v1:nightmare_claw macabre:eye arphex:venomous_appendage");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_8_potion arphex:mangled_spider_flesh bossominium:the_golden_eye born_in_chaos_v1:seedof_chaos born_in_chaos_v1:spider_mandible alexscaves:heavy_bone");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_7_potion bossominium:pure_pearl deeperdarker:soul_crystal arphex:mantis_machete mutantmonsters:endersoul_hand legendary_monsters:withered_bone");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_6_potion illageandspillage:spellbound_book bossominium:ancient_scrap born_in_chaos_v1:lifestealer_bone arphex:abyssal_shard bossominium:all_seeing_crystal");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_5_potion kom:sigil_of_revival soulsweapons:lord_soul_rose minecraft:nether_star cataclysm:witherite_ingot the_deep_void:void_matter");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_4_potion iceandfire:dragon_skull_lightning sleepy_hollows:spectral_essence the_deep_void:stalker_scythe_claw mythsandlegends:umbras_whisper macabre:gomoria_heart");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_3_potion iceandfire:dragon_skull_fire born_in_chaos_v1:lord_pumpkinheads_lamp arphex:fire_opal");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_2_potion terramity:fortunes_favor soulsweapons:lord_soul_day_stalker soulsweapons:lord_soul_night_prowler minecraft:spyglass");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_1_potion terramity:music_sheet_of_the_omnipotent_ultra_sniffer minecraft:emerald_block");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_9_potion mowziesmobs:sol_visage zoniex:deathly_onyx mowziesmobs:wrought_axe macabre:rattails deeperdarker:sculk_bone");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_8_potion aether:silver_dungeon_key bossominium:the_golden_eye terramity:spiteful_soul mutantmonsters:hulk_hammer macabre:blindbaloon_item");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_7_potion aether:gold_dungeon_key bosses_of_mass_destruction:blazing_eye macabre:mortis_essence arphex:mangled_fly_flesh bosses_of_mass_destruction:obsidian_heart");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_6_potion awakened_bosses:herobrine_nugget macabre:rootofinfestation the_ravenous:rav_egg born_in_chaos_v1:soul_cutlass minecraft:white_banner");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_5_potion soulsweapons:lord_soul_rose soulsweapons:essence_of_eventide animatedmobsmod:ender_spectre alexscaves:uranium soulsweapons:darkin_blade");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_4_potion alexscaves:tectonic_shard iceandfire:dragon_skull_lightning eeeabsmobs:guardian_core macabre:valamon_heart terramity:belt_of_the_gnome_king");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_3_potion iceandfire:dragon_skull_fire terramity:perish_staff arphex:abyssal_crystal");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_2_potion terramity:fortunes_favor soulsweapons:lord_soul_day_stalker soulsweapons:lord_soul_night_prowler minecraft:clock");
-        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_1_potion terramity:music_sheet_of_the_omnipotent_ultra_sniffer minecraft:gold_block");
+        // Monster Potions
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_9_potion ingredients 2 bossominium:flower_of_genesis bossominium:redstone_hard_drive minecraft:rotten_flesh alexscaves:charred_remnant bosses_of_mass_destruction:soul_star");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_8_potion ingredients 2 legendary_monsters:frozen_rune legendary_monsters:crystal_of_sandstorm mutantmonsters:hulk_hammer alexscaves:sweet_tooth minecraft:netherite_scrap");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_7_potion ingredients 2 alexscaves:pure_darkness bossominium:soul_eye arphex:giant_spinneret the_deep_void:ash_pile bossominium:dead_charm");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_6_potion ingredients 2 minecraft:nether_star bosses_of_mass_destruction:void_thorn kom:nectra_egg cataclysm:monstrous_horn illageandspillage:bag_of_horrors");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_5_potion ingredients 2 soulsweapons:chaos_crown cataclysm:witherite_ingot kom:anglospike arphex:crusher_claw alexscaves:immortal_embryo");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_4_potion ingredients 2 iceandfire:dragon_skull_fire cataclysm:ignitium_ingot eeeabsmobs:guardian_core mythsandlegends:umbras_whisper macabre:gargamaw_heart");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_3_potion ingredients 1 minecraft:nether_star iceandfire:dragon_skull_ice arphex:abyssal_crystal");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_2_potion ingredients 1 terramity:giant_sniffers_hoof soulsweapons:lord_soul_day_stalker soulsweapons:lord_soul_night_prowler minecraft:iron_ingot");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:monster_1_potion ingredients 1 terramity:music_sheet_of_the_omnipotent_ultra_sniffer minecraft:netherite_block");
+
+        // Sailor Potions
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_9_potion ingredients 2 bossominium:rusted_trident mowziesmobs:sol_visage aquamirae:fin arphex:roach_nymph arphex:fly_appendage");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_8_potion ingredients 2 bossominium:mossy_stone_tablet alexsmobs:warped_muscle iceandfire:sea_serpent_fang minecraft:prismarine_shard mutantmonsters:endersoul_hand");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_7_potion ingredients 2 eeeabsmobs:heart_of_pagan mowziesmobs:ice_crystal aquamirae:abyssal_amethyst the_deep_void:tendrils arphex:necrotic_fang");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_6_potion ingredients 2 illageandspillage:spellbound_book kom:caligan_saw arphex:oversized_stinger minecraft:dragon_egg bosses_of_mass_destruction:void_thorn");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_5_potion ingredients 2 aquamirae:frozen_key soulsweapons:essence_of_eventide cataclysm:gauntlet_of_guard alexscaves:immortal_embryo arphex:void_geode_shard");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_4_potion ingredients 2 iceandfire:dragon_skull_ice alexscaves:tectonic_shard cataclysm:abyssal_egg macabre:baal_heart soulsweapons:darkin_blade");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_3_potion ingredients 1 soulsweapons:essence_of_luminescence iceandfire:dragon_skull_lightning arphex:void_geode");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_2_potion ingredients 1 terramity:angel_feather soulsweapons:lord_soul_day_stalker soulsweapons:lord_soul_night_prowler minecraft:lightning_rod");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:sailor_1_potion ingredients 1 terramity:music_sheet_of_the_omnipotent_ultra_sniffer minecraft:diamond_block");
+
+        // Spectator Potions
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_9_potion ingredients 2 bossominium:golden_shard bossominium:forest_core born_in_chaos_v1:nightmare_claw macabre:eye arphex:venomous_appendage");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_8_potion ingredients 2 arphex:mangled_spider_flesh bossominium:the_golden_eye born_in_chaos_v1:seedof_chaos born_in_chaos_v1:spider_mandible alexscaves:heavy_bone");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_7_potion ingredients 2 bossominium:pure_pearl deeperdarker:soul_crystal arphex:mantis_machete mutantmonsters:endersoul_hand legendary_monsters:withered_bone");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_6_potion ingredients 2 illageandspillage:spellbound_book bossominium:ancient_scrap born_in_chaos_v1:lifestealer_bone arphex:abyssal_shard bossominium:all_seeing_crystal");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_5_potion ingredients 2 kom:sigil_of_revival soulsweapons:lord_soul_rose minecraft:nether_star cataclysm:witherite_ingot the_deep_void:void_matter");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_4_potion ingredients 2 iceandfire:dragon_skull_lightning sleepy_hollows:spectral_essence the_deep_void:stalker_scythe_claw mythsandlegends:umbras_whisper macabre:gomoria_heart");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_3_potion ingredients 1 iceandfire:dragon_skull_fire born_in_chaos_v1:lord_pumpkinheads_lamp arphex:fire_opal");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_2_potion ingredients 1 terramity:fortunes_favor soulsweapons:lord_soul_day_stalker soulsweapons:lord_soul_night_prowler minecraft:spyglass");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:spectator_1_potion ingredients 1 terramity:music_sheet_of_the_omnipotent_ultra_sniffer minecraft:emerald_block");
+
+        // Warrior Potions
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_9_potion ingredients 2 mowziesmobs:sol_visage zoniex:deathly_onyx mowziesmobs:wrought_axe macabre:rattails deeperdarker:sculk_bone");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_8_potion ingredients 2 aether:silver_dungeon_key bossominium:the_golden_eye terramity:spiteful_soul mutantmonsters:hulk_hammer macabre:blindbaloon_item");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_7_potion ingredients 2 aether:gold_dungeon_key bosses_of_mass_destruction:blazing_eye macabre:mortis_essence arphex:mangled_fly_flesh bosses_of_mass_destruction:obsidian_heart");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_6_potion ingredients 2 awakened_bosses:herobrine_nugget macabre:rootofinfestation the_ravenous:rav_egg born_in_chaos_v1:soul_cutlass minecraft:white_banner");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_5_potion ingredients 2 soulsweapons:lord_soul_rose soulsweapons:essence_of_eventide animatedmobsmod:ender_spectre alexscaves:uranium soulsweapons:darkin_blade");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_4_potion ingredients 2 alexscaves:tectonic_shard iceandfire:dragon_skull_lightning eeeabsmobs:guardian_core macabre:valamon_heart terramity:belt_of_the_gnome_king");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_3_potion ingredients 1 iceandfire:dragon_skull_fire terramity:perish_staff arphex:abyssal_crystal");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_2_potion ingredients 1 terramity:fortunes_favor soulsweapons:lord_soul_day_stalker soulsweapons:lord_soul_night_prowler minecraft:clock");
+        executeRecipeCommand(server, "/beyonderrecipe add lotm:warrior_1_potion ingredients 1 terramity:music_sheet_of_the_omnipotent_ultra_sniffer minecraft:gold_block");
     }
 
     public static void registerAbilities(Player player, MinecraftServer server) {
