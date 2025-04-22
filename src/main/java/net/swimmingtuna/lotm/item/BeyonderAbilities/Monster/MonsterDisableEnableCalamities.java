16bbd3b65eef06ffb9320b9c0f456e830d825c29
Storage4234234 <lione26@ucc.on.ca>
2025-04-22 14:02:34 -0400

    Update MonsterDisableEnableCalamities.java

--- src/main/java/net/swimmingtuna/lotm/item/BeyonderAbilities/Monster/MonsterDisableEnableCalamities.java
+++ src/main/java/net/swimmingtuna/lotm/item/BeyonderAbilities/Monster/MonsterDisableEnableCalamities.java
@@ -47,7 +47,7 @@ public class MonsterDisableEnableCalamities extends SimpleAbilityItem {
 
     @Override
     public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
-        tooltipComponents.add(Component.literal("Upon use, enables or disables your calamity attraction. If enabled, calamitites will be attracted to you"));
+        tooltipComponents.add(Component.literal("Upon use, toggles your calamity attraction. If enabled, calamities will be attracted to you."));
         tooltipComponents.add(Component.literal("Spirituality Used: ").append(Component.literal("None").withStyle(ChatFormatting.YELLOW)));
         tooltipComponents.add(Component.literal("Cooldown: ").append(Component.literal("1 Second").withStyle(ChatFormatting.YELLOW)));
         tooltipComponents.add(SimpleAbilityItem.getPathwayText(this.requiredClass.get()));
