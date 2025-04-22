e939dee2d777dea46246fc68f3e0012faef3367d
Storage4234234 <lione26@ucc.on.ca>
2025-04-22 13:54:29 -0400

    Update MisfortuneBestowal.java

--- src/main/java/net/swimmingtuna/lotm/item/BeyonderAbilities/Monster/MisfortuneBestowal.java
+++ src/main/java/net/swimmingtuna/lotm/item/BeyonderAbilities/Monster/MisfortuneBestowal.java
@@ -66,7 +66,7 @@ public class MisfortuneBestowal extends SimpleAbilityItem {
 
     @Override
     public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
-        tooltipComponents.add(Component.literal("Upon use, gives the target misfortune and takes away some of your own"));
+        tooltipComponents.add(Component.literal("Upon use, give the target misfortune and remove some of your own."));
         tooltipComponents.add(Component.literal("Spirituality Used: ").append(Component.literal("250").withStyle(ChatFormatting.YELLOW)));
         tooltipComponents.add(Component.literal("Cooldown: ").append(Component.literal("20 Seconds").withStyle(ChatFormatting.YELLOW)));
         tooltipComponents.add(SimpleAbilityItem.getPathwayText(this.requiredClass.get()));
