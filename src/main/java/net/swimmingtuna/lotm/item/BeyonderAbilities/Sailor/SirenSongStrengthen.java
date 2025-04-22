0b45d873f2696db3191c3f21f6de253866b5c951
Storage4234234 <lione26@ucc.on.ca>
2025-04-22 14:47:51 -0400

    Update SirenSongStrengthen.java

--- src/main/java/net/swimmingtuna/lotm/item/BeyonderAbilities/Sailor/SirenSongStrengthen.java
+++ src/main/java/net/swimmingtuna/lotm/item/BeyonderAbilities/Sailor/SirenSongStrengthen.java
@@ -92,7 +92,7 @@ public class SirenSongStrengthen extends SimpleAbilityItem {
 
     @Override
     public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
-        tooltipComponents.add(Component.literal("Upon use, sings out a song which strengthens you"));
+        tooltipComponents.add(Component.literal("Upon use, sings out a song which strengthens you."));
         tooltipComponents.add(Component.literal("Left Click for Siren Song Stun"));
         tooltipComponents.add(Component.literal("Spirituality Used: ").append(Component.literal("300").withStyle(ChatFormatting.YELLOW)));
         tooltipComponents.add(Component.literal("Cooldown: ").append(Component.literal("50 Seconds").withStyle(ChatFormatting.YELLOW)));
