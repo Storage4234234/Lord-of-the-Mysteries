ba96a8ea5e6ab73ca53977c6d153676ab00f026f
Storage4234234 <lione26@ucc.on.ca>
2025-04-22 13:59:25 -0400

    Update MisfortuneRedirection.java

--- src/main/java/net/swimmingtuna/lotm/item/BeyonderAbilities/Monster/MisfortuneRedirection.java
+++ src/main/java/net/swimmingtuna/lotm/item/BeyonderAbilities/Monster/MisfortuneRedirection.java
@@ -92,7 +92,7 @@ public class MisfortuneRedirection extends SimpleAbilityItem {
 
     @Override
     public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
-        tooltipComponents.add(Component.literal("Upon use, takes all misfortunate values from all entities nearby and causes them to instantly occur in the target"));
+        tooltipComponents.add(Component.literal("Upon use, takes all misfortunate events from all nearby entities and causes them to instantly occur on the target."));
         tooltipComponents.add(Component.literal("Spirituality Used: ").append(Component.literal("300").withStyle(ChatFormatting.YELLOW)));
         tooltipComponents.add(Component.literal("Cooldown: ").append(Component.literal("10 Seconds").withStyle(ChatFormatting.YELLOW)));
         tooltipComponents.add(SimpleAbilityItem.getPathwayText(this.requiredClass.get()));
